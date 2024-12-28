f1fcab5f7aa3335c1502724563f71f6e2a257084
ahmed.abdullah <ahmed.abdullah@zong.com.pk>
2024-12-28 15:50:48 +0500

    remove unused code

--- lib/src/presentation/screen/welcome/view_model/welcome_viewModel.dart
+++ lib/src/presentation/screen/welcome/view_model/welcome_viewModel.dart
@@ -1,24 +1,7 @@
-
-import 'dart:io';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'package:path/path.dart' as path;
 import 'package:movie/src/core/lifecycle/use_lifecycle.dart';
 
-
 import '../../../../core/utils/global_utils.dart';
-class WelcomeViewModel extends UseLifeCycle with ChangeNotifier {
 
-    WelcomeViewModel(){
-
-
-    }
-
-
-
-
-
-}
+class WelcomeViewModel extends UseLifeCycle with ChangeNotifier {}
